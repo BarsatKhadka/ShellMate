0f6fa3afa43b151278e4300a8a089d3cf10060f9
BarsatKhadka <khadkabarsat598@gmail.com>
2025-04-05 08:52:51 -0500

    add: Validating open ai api key with open ai client

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import typer
 import os
+from openai import OpenAI
 
 app = typer.Typer()
 
@@ -10,15 +11,29 @@ def shellmate():
 
 
 
+
 def check_api_key():
     api_key = os.getenv("OPENAI_API_KEY")
 
     if not api_key:
-        print("Set the OPENAI_API_KEY in your environment variable.")
-        print("Instructions provided in the README.")
+        print("❌ OPENAI_API_KEY is not set.")
+        print("👉 Set it in your environment as shown in the README.")
         return False
 
-    return True   
+    client = OpenAI(api_key=api_key)
+    try:
+        client.models.list()
+        print("✅ API key is valid.")
+        return True
+    except Exception as e:
+        if(e.code == 'invalid_api_key'):
+            print("❌ Invalid API key.")
+            print("👉 Check your API key and try again.")
+        else:
+            print("❌ An error occurred while validating the API key.")
+            print(f"Error details. {e}")
+        return False
+    
     
     
 
