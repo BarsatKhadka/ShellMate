d05d90341c6eecac0c702edf4e1a91efc94e90d0
BarsatKhadka <khadkabarsat598@gmail.com>
2025-04-05 08:23:26 -0500

    add: check api key function

--- main.py
+++ main.py
@@ -5,11 +5,20 @@ app = typer.Typer()
 
 @app.command()
 def shellmate():
+    if check_api_key():
+        print("Shellmate is running...")
+
+
+
+def check_api_key():
     api_key = os.getenv("OPENAI_API_KEY")
 
     if not api_key:
         print("Set the OPENAI_API_KEY in your environment variable.")
         print("Instructions provided in the README.")
+        return False
+
+    return True   
     
     
 
